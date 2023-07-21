eebee613651473ee7a234929abad8daab6f685f9
yangharry <myid7771@naver.com>
2023-07-21 16:03:47 +0900

    no rest

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -71,7 +71,7 @@
             <q-icon v-else name="none" />
           </div>
         </div>
-        <div v-if="a.performance_time < 0" class="col flex flex-center ">No Rest
+        <div v-if="a.performance_time < 0" class="col flex flex-center " style="font-size: 12px;">No Rest
         </div>
         <div v-else class="col flex flex-center ">{{ min(a.performance_time) }}:{{ sec(a.performance_time) }}
         </div>
